a93b2815d575730a89c80fd7b37c5b12f7e20fc5
Antoine <antoine.favreau84@gmail.com>
2020-04-16 18:21:33 +0200

    Add checkboxes for outing reasons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CreateCertificateButton from './components/CreateCertificateButton'
+import OutingReasonsCheckboxes from './components/OutingReasonsCheckboxes'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <OutingReasonsCheckboxes/>
        <CreateCertificateButton/>
     </div>
   );
---
+++ src/components/OutingReasonsCheckboxes.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {FormControl, FormLabel, FormControlLabel, Checkbox, FormGroup} from '@material-ui/core'
+
+const outingReasons = [
+    {id:'work', title:'Travail'},
+    {id:'shopping', title:'Courses'},
+    {id:'health', title:'Santé'},
+];
+
+const useStyles = makeStyles((theme) =>({
+    formControl: {
+        margin: theme.spacing(3)
+    }
+}))
+
+const OutingReasonsCheckboxes = function(){
+    
+    const getInitialState = function(){
+        let initialState = {}
+        outingReasons.forEach(reason => {
+            initialState = {...initialState,[reason.id]:false}
+        });
+        
+        return initialState;
+    }
+    const [state, setState] = React.useState(getInitialState())
+
+    const classes = useStyles();
+
+    const handleChange = function(event){
+        setState({...state,[event.target.name]:event.target.checked})
+    }
+    return(
+        <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Motifs de déplacement</FormLabel>
+        <FormGroup>
+            {outingReasons.map(reason => {
+                return (
+                    <FormControlLabel key={reason.id}
+                    control={<Checkbox 
+                                checked={state[reason.id]} 
+                                onChange={handleChange} 
+                                name={reason.id} 
+                            />}
+                    label={reason.title}
+                  />
+                )
+            })}
+        </FormGroup>
+      </FormControl>
+    )
+}
+
+export default OutingReasonsCheckboxes
