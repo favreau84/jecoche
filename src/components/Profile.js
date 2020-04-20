fc7a5acfc1b6dba5011317ac6566834e07d7f255
Antoine <antoine.favreau84@gmail.com>
2020-04-20 08:49:52 +0200

    fix styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,6 +10,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
+      width:300
     },
   },
 }));
@@ -35,71 +36,81 @@ function Profile(props){
 
     return (
         <div className={classes.root}>
-            <Grid container justify='flex-end'>
-                <Grid item>
-                    <IconButton onClick={handleClose}>
-                        <Close/>
-                    </IconButton>
+        <Grid container direction="column">
+            <Grid item>
+                <Grid container justify='flex-end'>
+                        <Grid item>
+                            <IconButton onClick={handleClose}>
+                                <Close/>
+                            </IconButton>
+                        </Grid>
                 </Grid>
             </Grid>
-
-            <form>
-                <div>
-                    <TextField id="firstName" 
-                    label="Prénom" 
-                    variant="filled" 
-                    onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
-                    value={profile.firstName}/>
-                </div>
-                <div>
-                    <TextField id="lastName" 
-                    label="Nom" 
-                    variant="filled" 
-                    onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
-                    value={profile.lastName}/>
-                </div>
-                <div>
-                    <TextField id="birthDate" 
-                    label="Date de naissance" 
-                    variant="filled" 
-                    onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
-                    value={profile.birthDate}/>
-                </div>
-                <div>
-                    <TextField id="birthPlace" 
-                    label="Lieu de naissance" 
-                    variant="filled" 
-                    onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
-                    value={profile.birthPlace}/>
-                </div>
-                <div>
-                    <TextField id="addressStreet" 
-                    label="Adresse" 
-                    variant="filled" 
-                    onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
-                    value={profile.addressStreet}/>
-                </div>
-                <div>
-                    <TextField id="addressZipcode" 
-                    label="Code postal"
-                    type="number" 
-                    variant="filled" 
-                    onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
-                    value={profile.addressZipcode}/>
-                </div>
-                <div>
-                    <TextField id="addressCity" 
-                    label="Ville" 
-                    variant="filled" 
-                    onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
-                    value={profile.addressCity}/>
-                </div>
-                <div>
-                    <Button onClick={handleProfileSubmit} variant="contained" color='primary' > Enregistrer </Button>
-                </div>   
-            </form>
-            
-        </div>
+            <Grid item>
+                <form>
+                    <div>
+                        <TextField id="firstName" 
+                        label="Prénom" 
+                        variant="filled" 
+                        onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
+                        value={profile.firstName}/>
+                    </div>
+                    <div>
+                        <TextField id="lastName" 
+                        label="Nom" 
+                        variant="filled" 
+                        onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
+                        value={profile.lastName}/>
+                    </div>
+                    <div>
+                        <TextField id="birthDate" 
+                        label="Date de naissance" 
+                        variant="filled" 
+                        onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
+                        value={profile.birthDate}/>
+                    </div>
+                    <div>
+                        <TextField id="birthPlace" 
+                        label="Lieu de naissance" 
+                        variant="filled" 
+                        onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
+                        value={profile.birthPlace}/>
+                    </div>
+                    <div>
+                        <TextField id="addressStreet" 
+                        label="Adresse" 
+                        variant="filled" 
+                        onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
+                        value={profile.addressStreet}/>
+                    </div>
+                    <div>
+                        <TextField id="addressZipcode" 
+                        label="Code postal"
+                        type="number" 
+                        variant="filled" 
+                        onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
+                        value={profile.addressZipcode}/>
+                    </div>
+                    <div>
+                        <TextField id="addressCity" 
+                        label="Ville" 
+                        variant="filled" 
+                        onChange={({target: {id,value}})=>handleFieldChange(id,value)} 
+                        value={profile.addressCity}/>
+                    </div>   
+                </form>
+            </Grid>
+            <Grid item>
+                <Grid container justify='flex-end'>
+                    <Grid item>
+                        <Button style={{margin:'16px'}} onClick={handleProfileSubmit} variant="contained" color='primary'>     
+                        Enregistrer 
+                    </Button>
+                    </Grid>
+                </Grid>
+            </Grid>   
+        </Grid> 
+    </div>
     )
 }
 
